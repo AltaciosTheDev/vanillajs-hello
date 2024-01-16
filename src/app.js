61ba07691fb5536d3f5dda34547919bae3281ea1
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-01-16 00:13:58 +0000

    app.js corregido. Eliminado el que estaba malo.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+let protocol = 'https://';
+let pronouns = ['the', 'our'];
+let adjectives = ['great', 'big'];
+let nouns = ['jogger', 'racoon'];
+let topLevelDomain = ['.com', '.gov', '.edu', '.org', '.net', '.dev'];
+
+for(let pronoun of pronouns){
+    // first level
+    for(let adjective of adjectives){
+        // second level
+        for(let noun of nouns){
+            // third level
+            let random = Math.floor(Math.random() * topLevelDomain.length);
+            let domain = protocol + pronoun + adjective + noun + topLevelDomain[random];
+            console.log(domain);
+        }
+    }
+
+}
